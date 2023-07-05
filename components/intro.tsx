970c7689905e21ad3249c3b7ed63c33fdb48a9c7
evmKnows <github25234.nftu3@simplelogin.co>
2023-07-05 19:47:07 +0300

    Responsiveness fix + css in the right place now

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,25 +2,25 @@ import { CMS_NAME } from '../lib/constants'
 
 const Intro = () => {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex flex-col md:flex-row items-center justify-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         LlamaRisk
       </h1>
       
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+      <div className="flex flex-col md:flex-row justify-center items-center md:items-start mt-5 md:mt-0">
             <a
               href="https://cryptorisks.substack.com/archive"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 md:px-6 md:py-3 w-32 md:w-auto max-w-full text-center duration-200 transition-colors mb-3 md:mb-0"
             >
               Our reviews
             </a>
             <a
               href="https://twitter.com/LlamaRisk"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-2 px-4 md:px-6 md:py-3 w-32 md:w-auto max-w-full text-center duration-200 transition-colors mb-3 md:mb-0"
             >
               Twitter
             </a>
-      </h4>
+      </div>
       
     </section>
   )
